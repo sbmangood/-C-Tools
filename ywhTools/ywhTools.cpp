98de1e723f8873a5c428496a9c0338be7495a3a3
yuwanghai1988 <243302201@qq.com>
2020-09-07 14:44:58 +0800

    第一个可用版本: 1.还是TCPclient得依赖boost库,这个后面要优化 2.消息队列得优化,无锁的或者消费者批量处理消息方式

--- ywhTools/ywhTools.cpp
+++ ywhTools/ywhTools.cpp
@@ -1,13 +1,19 @@
 ﻿// ywhTools.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include "toolsTest\test1.h"
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	testEndpointAndTcpChannel();
+	system("pause");
 }
+	
+	
 
+	
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
